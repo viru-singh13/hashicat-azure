fcc05f197e60e706a45e42fce8fb81d1d65a70ce
veerbal singh <viru.singh13@gmail.com>
2023-05-03 08:44:28 +0000

    Added network module

--- variables.tf
+++ variables.tf
@@ -76,3 +76,9 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "resource_group_name" {
+  description = "The name of an existing resource group to be imported."
+  default    = "Veerbal"
+  type        = string
+}
